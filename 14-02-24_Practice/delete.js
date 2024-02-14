f443c2cbac2833310fdbee75f59ddc1223b7b185
ShopnilIsCoding <shopnilkhan766@gmail.com>
2024-02-14 23:54:35 +0600

    delete functionally added

--- 14-02-24_Practice/delete.js
+++ 14-02-24_Practice/delete.js
@@ -1,12 +1,45 @@
-document.getElementById('delete').addEventListener('click',function(){
+document.getElementById("delete").addEventListener('click',function(){
+    
     const target=document.getElementById('id');
-    const id = target.value;
-    const pr=document.querySelectorAll('.p') 
+    const id = parseInt(target.value);
+    const pr=document.querySelectorAll('.pr') 
     if (pr.length==0)
     {
-        console.log("empty")
+        alert('Comment Box Empty')
+        return
     }
-    else
-    console.log(pr.length)
+    
+    if(id>count)
+    {
+        alert('Your Input Id has not commented Yet')
+        return
+    }
+    //pr[id-1].parentNode.removeChild(pr[id-1])
+    let Found=true;
+    for (let i = 0; i < pr.length; i++) {
+        const p = pr[i];
+        const ptext = p.innerText;
+        
+        
+        if (ptext.includes(id.toString())) {
+            p.parentNode.removeChild(p);
+            return; 
+        }
+        else
+        {
+             Found=false;
+        }
+    }
+    if(!Found)
+    {
+        alert('Id Not Matched Or Already Deleted')
+    }
+})
 
+
+
+
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    
+    event.preventDefault();
 })
