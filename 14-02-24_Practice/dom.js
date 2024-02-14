6f38c959e2db6c4d8a16d223edc23f6893a8331b
ShopnilIsCoding <shopnilkhan766@gmail.com>
2024-02-14 20:36:52 +0600

    comment box complete

---
+++ 14-02-24_Practice/dom.js
@@ -0,0 +1,21 @@
+let count = 0
+document.getElementById("Post").addEventListener('click', function () {
+
+
+    const commentbox = document.getElementById('comment');
+    const namebox = document.getElementById('name');
+    if (commentbox.value === '' || namebox.value === '') {
+        alert("Fill Both Box");
+        return;
+    }
+    const comment = '<span style="font-weight: bold;">Customer ' + (++count) + '</span> ' + '<span style="font-weight: bold; color: tomato;">'+  namebox.value + '</span> Commented: <span style="color: blueviolet;">' + commentbox.value + '</span>';
+
+    const textbox = document.getElementById('text');
+    const p = document.createElement('p');
+    p.innerHTML = comment;
+    
+    
+    textbox.appendChild(p);
+    commentbox.value = '';
+    namebox.value = '';
+})
