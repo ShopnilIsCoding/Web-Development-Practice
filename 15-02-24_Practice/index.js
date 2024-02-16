3126be13e85d3d4261f879f24cf9e06d1c8eca20
ShopnilIsCoding <shopnilkhan766@gmail.com>
2024-02-16 15:15:45 +0600

    Bmi Calculator done

--- 15-02-24_Practice/index.js
+++ 15-02-24_Practice/index.js
@@ -5,6 +5,7 @@ document.getElementById('btn').addEventListener('click', function() {
     let getname=document.getElementById('name').value;
     let progress=document.getElementById('progress');
     let radial=document.getElementById('radial')
+    let cover=document.getElementById('cover')
     
     let name=getname.toUpperCase();
     let kg = parseInt(getkg);
@@ -26,23 +27,56 @@ document.getElementById('btn').addEventListener('click', function() {
     document.getElementById('feet').value = '';
     document.getElementById('name').value = '';
     progress.setAttribute('value',`${sbmi}`)
-    radial.setAttribute('style',`--value:${(sbmi*100)/40}`)
+    radial.setAttribute('style',`--value:${(sbmi*100)/40};--size:12rem; --thickness: 2rem;`)
     radial.innerText=`${sbmi}`;
-    const p=document.getElementById('span')
-    p.style.color='pink';
+    const p=document.createElement('span')
+    
     p.style.fontSize='1rem';
+    p.style.fontWeight='700';
+    p.style.textAlign='center';
     //p.innerText=name+' Your BMI is :'+bmi;
-    if(bmi>=17 && bmi<=23)
+    if(bmi>=18.50 && bmi<=25.00)
     {
-        p.innerText=name+' Your BMI is :'+bmi + '. You are rittik roshan';
+        p.innerText=name+' তুমি তো মিয়া রিত্তিক রোসান,অডিশন কবে? ';
         radial.style.color='lightgreen'
         p.style.color='lightgreen'
+        cover.setAttribute('style',`background-image: url('./hrithik-roshan-oq74orkp625wlwu2.jpg'); background-repeat: no-repeat; background-size: cover;background-position: center; object-fit: cover;`)
     }
-    else if(bmi>23 && bmi<=30)
+    else if(bmi>25.00 && bmi<=29.00)
     {
-        p.innerText=name+' Your BMI is : '+bmi + ' .  Tomar to vuri dkha jai';
+        p.innerText=name+'কি অবস্থা ব্রো? তোমার তো ভুড়ি দেখা যায়';
         radial.style.color='yellow'
         p.style.color='yellow'
+        cover.setAttribute('style',`background-image: url('./overweight.jpg'); background-repeat: no-repeat; background-size: cover;background-position: center; object-fit: cover;`)
+    }
+    else if(bmi>29 && bmi<=35)
+    {
+        p.innerText=name+'তোমার পেট থেকে তো কিছুদিন পর নতুন শিশু জন্ম নিবে ব্রো';
+        radial.style.color='orange'
+        p.style.color='orange'
+        cover.setAttribute('style',`background-image: url('./obesity-1.jpg'); background-repeat: no-repeat; background-size: cover;background-position: center; object-fit: cover;`)
+    }
+    else if(bmi>35.00 && bmi<=40.00)
+    {
+        p.innerText=name+'তোমার বোধ হয় আর রক্ষে নেই';
+        radial.style.color='pink'
+        p.style.color='pink'
+        cover.setAttribute('style',`background-image: url('./obesity-2.jpg'); background-repeat: no-repeat; background-size: cover;background-position: center; object-fit: cover;`)
+    }
+    else if(bmi>40)
+    {
+        p.innerText=name+'আরেহ! তুমি বেচে আছ কিভাবে?';
+        radial.style.color='red'
+        p.style.color='red'
+        cover.setAttribute('style',`background-image: url('./dead.jpg'); background-repeat: no-repeat; background-size: cover;background-position: center; object-fit: cover;`)
+    }
+    else
+    {
+        p.innerText=name+'ঝড় ,তুফান উঠলে নিজেকে সামলে রেখ উড়ে যেতে পার';
+        radial.style.color='blueviolet'
+        p.style.color='blueviolet'
+        cover.setAttribute('style',`background-image: url('./ronaldo.jpg'); background-repeat: no-repeat; background-size: cover;background-position: center; object-fit: cover;`)
+        
     }
     const result=document.getElementById('result');
     result.appendChild(p);
