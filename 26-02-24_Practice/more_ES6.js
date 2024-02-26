b12c51c6b95f2b11a1be1acf1a962c566b88fe11
ShopnilIsCoding <shopnilkhan766@gmail.com>
2024-02-26 15:07:26 +0600

    reduce,map,filter,find,foreach

--- 26-02-24_Practice/more_ES6.js
+++ 26-02-24_Practice/more_ES6.js
@@ -1,1 +1,7 @@
- 
+ const players=[12,43,21,65,332];
+//  const result=players.map(callback)
+//  const result=players.forEach(return_Nothing_But_Map)
+//  const result=players.filter(condition)
+//  const result=players.find(specificCondition)
+const sum=players.reduce((sum,number)=>sum+number,0);
+console.log(sum);
