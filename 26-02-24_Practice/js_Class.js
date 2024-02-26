b5b3eb7b424e4f1ee8861ba836f57c2739e38772
ShopnilIsCoding <shopnilkhan766@gmail.com>
2024-02-26 22:18:29 +0600

    cleared class functionality

--- 26-02-24_Practice/js_Class.js
+++ 26-02-24_Practice/js_Class.js
@@ -29,3 +29,69 @@ class bus extends vehicle {
         this.seat = seat;
     }
 }
+
+class pet{
+    constructor(name)
+    {
+        this.name=name;
+    }
+    showinfo()
+    {
+        console.log(`this is my pet name is ${this.name} `);
+    }
+}
+
+const cat=new pet('abc');
+cat.showinfo();
+
+
+class grandFather
+{
+    constructor(name,age)
+    {
+        this.gname=name;
+        this.gage=age;
+    }
+    
+}
+
+class Father extends grandFather
+{
+    constructor(fname,fage,gname,gage)
+    {
+        super(gname,gage);
+        this.fname=fname;
+        this.fage=fage;
+    }
+    
+}
+
+class Son extends Father
+{
+    constructor(name,age,fname,fage,gname,gage)
+    {
+        super(fname,fage,gname,gage);
+        this.name=name;
+        this.age=age;
+    }
+    MyInfo() {
+        console.log(`My name is ${this.name} and my age is ${this.age}`);
+    }
+
+    FatherInfo() {
+        console.log(`My father name is ${this.fname} and his age is ${this.fage}`);
+    }
+
+    grandFatherInfo() {
+        console.log(`My grandfather name is ${this.gname} and his age is ${this.gage}`);
+    }
+
+    showAllInfo() {
+        this.MyInfo();
+        this.FatherInfo();
+        this.grandFatherInfo();
+    }
+}
+
+const Information=new Son('Rahomotul Islam',23,'Rafiqul Islam',58,'Ishaq Ali Khan',92);
+Information.showAllInfo();
