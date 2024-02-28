cbe2ce6286fda46963d666ac0f474133c7a4df85
ShopnilIsCoding <shopnilkhan766@gmail.com>
2024-02-28 20:52:01 +0600

    Crud

--- 28-02-24_Practice/json.js
+++ 28-02-24_Practice/json.js
@@ -1,3 +1,31 @@
-const json='{"abcd" : "1" }'
-const js=JSON.parse(json);
-console.log(js);
+// const json='{"abcd" : "1" }'
+// const js=JSON.parse(json);
+// console.log(js);
+const url='https://jsonplaceholder.typicode.com/users';
+function LoadData()
+{
+    
+    fetch(url)
+    .then(r=>r.json())
+    .then(data=>consoledata(data))
+    
+}
+
+//method-2
+const LoadData2=async()=>{
+    const res= await fetch(url);
+    const data=await res.json();
+    consoledata(data);
+}
+
+
+function consoledata(data)
+{
+    const ul=document.getElementById('ul');
+    for (const user of data) {
+        const li=document.createElement('li');
+        li.innerText=user.email;
+        ul.appendChild(li);
+    }
+   
+}
