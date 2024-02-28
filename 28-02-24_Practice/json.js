b2e1f91b0126d083661518a030756b2783452072
ShopnilIsCoding <shopnilkhan766@gmail.com>
2024-02-28 20:55:23 +0600

    CRUD,JSON

--- 28-02-24_Practice/json.js
+++ 28-02-24_Practice/json.js
@@ -1,31 +1,35 @@
 // const json='{"abcd" : "1" }'
 // const js=JSON.parse(json);
 // console.log(js);
-const url='https://jsonplaceholder.typicode.com/users';
-function LoadData()
-{
-    
+const url = 'https://jsonplaceholder.typicode.com/users';
+function LoadData() {
+
     fetch(url)
-    .then(r=>r.json())
-    .then(data=>consoledata(data))
-    
+        .then(r => r.json())
+        .then(data => consoledata(data))
+        .catch(error => console.error('Ki VUlVal korish', error));
+
 }
 
 //method-2
-const LoadData2=async()=>{
-    const res= await fetch(url);
-    const data=await res.json();
-    consoledata(data);
+const LoadData2 = async () => {
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        consoledata(data);
+    }
+    catch (error) {
+        console.error('Vul hoise vai tor');
+    }
 }
 
 
-function consoledata(data)
-{
-    const ul=document.getElementById('ul');
+function consoledata(data) {
+    const ul = document.getElementById('ul');
     for (const user of data) {
-        const li=document.createElement('li');
-        li.innerText=user.email;
+        const li = document.createElement('li');
+        li.innerText = user.email;
         ul.appendChild(li);
     }
-   
+
 }
