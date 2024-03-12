be52d0090386d8453d3b6d63eac0d81fcddadc87
ShopnilIsCoding <shopnilkhan766@gmail.com>
2024-03-13 03:07:50 +0600

    array mapping in react

--- 12-03-24_Practice/src/App.jsx
+++ 12-03-24_Practice/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Person from './todo'
 
 function App() {
   const [count, setCount] = useState(0)
 
+
+  const details=[
+    {job:'Home-Tutor',age:22},
+    {age:32},
+    {job:'Doctor',age:54},
+    {job:'Engineer',age:43}
+  ]
   return (
     <>
       
@@ -15,10 +20,18 @@ function App() {
       <Person name='kuru' device='tablet' price='33k' isrich={false}></Person>
       <Person name='bulu' device='laptop' price='73k' isrich={true}></Person>
       <Person name='kulu' device='desktop' price='113k' isrich={true}></Person>
-      
+      {
+        details.map(details=><Details job={details.job} age={details.age}></Details>)
+      }
     </>
   )
 }
 
+function Details({job,age})
+{
+ return (<>
+ <h4>his job is {job?job:'unemployed'} and his age is {age}</h4>
+ </>)
+}
 
 export default App
