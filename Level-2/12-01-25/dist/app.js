17dc8f9572bfe4a4c32a594846ae0019759b6b6e
ShopnilIsCoding <shopnilkhan766@gmail.com>
2025-01-12 00:01:41 +0600

    middleware created

--- Level-2/12-01-25/dist/app.js
+++ Level-2/12-01-25/dist/app.js
@@ -5,7 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.get('/', (req, res) => {
+//parser
+app.use(express_1.default.json());
+//middleware
+const appMiddleware = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
+app.get('/', appMiddleware, (req, res) => {
     res.send('Hello World!');
+});
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.json({ message: 'Hello World from POST request!' });
 });
 exports.default = app;
--- Level-2/12-01-25/src/app.ts
+++ Level-2/12-01-25/src/app.ts
@@ -1,9 +1,24 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 const app = express()
+//parser
+app.use(express.json());
 
+//middleware
 
-app.get('/', (req, res) => {
+const appMiddleware = (req:Request, res:Response, next:NextFunction) =>
+{
+    console.log(req.url,req.method,req.hostname);
+    next();
+}
+
+app.get('/', appMiddleware, (req:Request, res:Response) => {
   res.send('Hello World!')
+})
+
+app.post('/',(req:Request,res:Response)=>
+{
+    console.log(req.body);
+    res.json({message:'Hello World from POST request!'})
 })
 
 export default app;
